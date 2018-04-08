55c54454852337cd29320353480d8a71f1eb3302
Joel Weidenmark <jweidenmark@hotmail.com>
2018-04-08 18:59:23 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+$(function() {
+
+
+});
+
+
+$(function() {
+	var view = new SiteView(document);
+	//var view = new SiteView($("#wrapper"));
+	var controller = new SiteController($("#wrapper"), view);
+});
+
---
+++ js/controller.js
@@ -0,0 +1,14 @@
+var SiteController = function (container, view) {
+	
+
+	/*
+	var createDinnerButton = container.find("#createDinnerbtn");
+
+	$(document).ready(function(){
+		$(createDinnerButton).click(function(){
+			app.showDinnerOverview();
+			model.searchQueryId("", "")
+		});
+	});
+	*/
+};
