e1c88730fa39c735b18c257f97d5d2628d29cea5
Joel Weidenmark <jweidenmark@hotmail.com>
2018-04-08 22:22:52 +0200

    Lab 1.2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 
-$(function() {
+function hideShow() {
+    var front = document.getElementById("frontPage");
+    var wrapper = document.getElementById("mainMap");
+    front.style.display = "none";
+    wrapper.style.display = "block";
 
-
-});
-
-
-$(function() {
-	var view = new SiteView(document);
-	//var view = new SiteView($("#wrapper"));
-	var controller = new SiteController($("#wrapper"), view);
-});
+    /*
+    if (wrapper.style.display === "none") {
+    	front.style.display = "none";
+        wrapper.style.display = "block";
+    } else {
+        wrapper.style.display = "none";
+    }
+    */
+}
 
